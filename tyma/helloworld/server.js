80f8de1f3693e6e061a32c45ea4ba6ad0fbba5c4
Mr. Blune <kontakt@thebluneproject.de>
2015-11-22 01:33:07 +0100

    tst2

--- tyma/helloworld/server.js
+++ tyma/helloworld/server.js
@@ -1,6 +1,2 @@
-var http = require('http');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+/* Hello, World! program in node.js */
+console.log("Hello, World!")
